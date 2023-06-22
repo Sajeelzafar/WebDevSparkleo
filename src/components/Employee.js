1444dee231bb3e67d7578d4ce83556519986736b
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-06-22 21:19:44 +0500

    Adding styling to employee form

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -5,27 +5,23 @@ const Addemployee = () => {
     return(
         <form className="form display">
             <div className="formHeadingContainer">
-                <h3 className="formHeading">New Employee</h3>
+                <h5 className="formHeading">New Employee</h5>
             </div>
 
-            <div className="formInputContainer display">
-                <label> First Name
-                    <input className="firstname" type="text" placeholder="First Name"/>
-                </label>
-                <label> Last Name
-                    <input className="lastname" type="text" placeholder="Last Name"/>
-                </label>
-                <label>Email
-                    <input className="email" type="email" placeholder="Email"/>
-                </label>
-                <label> Phone
-                    <input className="phone" type="number" placeholder="Phone Number"/>
-                </label>
+            <div className="formInputContainer grid">
+                <label> First Name <span class="red-star">*</span> </label>
+                <input className="firstname" type="text" placeholder="First Name" required/>
+                <label> Last Name <span class="red-star">*</span> </label>
+                <input className="lastname" type="text" placeholder="Last Name" required/>
+                <label>Email <span class="red-star">*</span> </label>
+                <input className="email" type="email" placeholder="Email" required/>
+                <label> Phone <span class="red-star">*</span> </label>
+                <input className="phone" type="number" placeholder="Phone Number" required/>
             </div>
 
             <div className="formButtons display">
-                <button>CLEAR ALL</button>
-                <button>ADD</button>
+                <button className="clearButton">CLEAR ALL</button>
+                <button className="addButton">ADD</button>
             </div>
         </form>
     )
